e399e9852f589a7837ff7e528b522529611f3b86
Jon Tran <trungt8998@gmail.com>
2020-04-17 10:58:28 -0500

    Gatsby data with Graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Crash Course`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@Jon Tran`,
+    author: `Jon Tran`,
   },
   plugins: [
     `gatsby-plugin-sass`,
