4a504d21d08881640aacc2ce694a8f22ca226ac8
Jon Tran <trungt8998@gmail.com>
2020-04-18 15:18:33 -0500

    SEtting head react-helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const IndexPage = () => {
   return(
     <Layout>
+      <Head title="Jon Tran"></Head>
       <h1>Hello World</h1>
       <h2>I'm Jon Tran, a font-end developer living in a beautiful Winnipeg city, Canada</h2>
     </Layout>
